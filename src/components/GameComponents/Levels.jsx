f5f9effb1f6cd6afe85ce564a2e5ba05e346d7e7
Husamoa <pawebial@gmail.com>
2018-08-28 23:09:53 +0200

    formatting code

--- src/components/GameComponents/Levels.jsx
+++ src/components/GameComponents/Levels.jsx
@@ -1,7 +1,8 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from "react-router-dom";
 
-import { updateSession, loadOrCreateNewSession } from "./UserSession";
+import {updateSession, loadOrCreateNewSession} from "./UserSession";
+import EndOfGame from "./EndOfGame";
 
 
 export default class Levels extends Component {
@@ -47,27 +48,47 @@ export default class Levels extends Component {
         })
     };
 
+
+
     goToNextLevel = () => {
         loadOrCreateNewSession().then(session => {
-            session.progress.finishedLevels.push(parseInt(this.props.match.params.id, 10));
-            session.progress.finishedLevels = session.progress.finishedLevels.filter(Boolean);
-            updateSession(session).then(() => {
+            if (session.progress.finishedLevels.includes(Number(this.props.match.params.id))) {
                 let url = `/level/${Number(this.props.match.params.id) + 1}`;
                 this.props.history.push(url);
-            })
+            } else {
+                session.progress.finishedLevels.push(parseInt(this.props.match.params.id, 10));
+                session.progress.finishedLevels = session.progress.finishedLevels.filter(Boolean);
+                updateSession(session).then(() => {
+                    let url = `/level/${Number(this.props.match.params.id) + 1}`;
+                    this.props.history.push(url);
+                })
+            }
         });
+    };
 
-
+    endOrNextLevel = () => {
+        return fetch(`/questions`).then(resp => {
+            return resp.json();
+        }).then((data) => {
+            if (Number(this.props.match.params.id) >= data.length) {
+                return (
+                    this.props.history.push('/end-of-game')
+                )
+            } else {
+                this.goToNextLevel();
+            }
+        }).catch(err => {
+            console.log('Błąd', err);
+        });
     };
 
     onSubmit = () => {
-            if (this.state.answer === this.state.userAnswer) {
-                console.log('poprawna odpowiedź');
-                this.goToNextLevel()
-
-            } else {
-                console.log('błędna odpowiedź');
-            }
+        if (this.state.answer === this.state.userAnswer) {
+            console.log('poprawna odpowiedź');
+            this.endOrNextLevel();
+        } else {
+            console.log('błędna odpowiedź');
+        }
     };
 
     handleKeyPress = e => {
@@ -83,21 +104,24 @@ export default class Levels extends Component {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-10'>
-                            <h1>To jest ekran kolejnych zadań.</h1>
+                            <h1></h1>
                         </div>
                         <div className='col-2'>
-                            <Link to='/levels-progress'><button className='btn btn-info my-btn-info'> Powrót do mapy</button></Link>
+                            <Link to='/levels-progress'>
+                                <button className='btn btn-info my-btn-info'> Powrót do mapy</button>
+                            </Link>
                         </div>
                     </div>
 
-                    <h2>Przykładowe zadanie {this.props.match.params.id}.</h2>
+                    <h2>Zagadka {this.props.match.params.id}.</h2>
                     <div>
                         {this.state.question}
                     </div>
                     <input type="text" className="form-control" aria-label="Large"
                            aria-describedby="inputGroup-sizing-sm" value={this.state.userAnswer}
-                           onChange={this.onChangeAnswer} onKeyPress={this.handleKeyPress} ref={input => input && input.focus()}/>
-                    <div className="input-group-append" >
+                           onChange={this.onChangeAnswer} onKeyPress={this.handleKeyPress}
+                           ref={input => input && input.focus()}/>
+                    <div className="input-group-append">
                         <button onClick={() => this.onSubmit()}
                                 className="btn btn-outline-secondary"
                                 type="button">Sprawdź
